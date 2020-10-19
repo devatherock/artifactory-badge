8dad75cddbf768170c14cea804e56c02e1562e4b
devatherock <devatherock@gmail.com>
2020-10-19 05:35:38 -0500

    Hardcode shields.io URL
    Required as config placeholders do not resolve to values already defined in `@ConfigurationProperties`

--- src/main/java/io/github/devatherock/artifactory/service/ShieldsIOClient.java
+++ src/main/java/io/github/devatherock/artifactory/service/ShieldsIOClient.java
@@ -4,7 +4,7 @@ import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.PathVariable;
 import io.micronaut.http.client.annotation.Client;
 
-@Client(value = "${artifactory.badge.shields-io.url}")
+@Client(value = "https://img.shields.io/badge")
 public interface ShieldsIOClient {
 	
 	@Get(value = "/{fileName}")
